543ecbba2d65c566e056b5fdea9dde17dd8a1670
luissifu@work <luis.e.sifuentes@gmail.com>
2014-10-30 17:27:32 -0600

    added external library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "Game.h"
 
+#include <iostream>
+#include "libs/json/json.h"
+
 int main() {
+	Json::Value root;
+	Json::Reader reader;
+
+	if (!reader.parse("test.json", root ))
+	{
+		// report to the user the failure and their locations in the document.
+		std::cout  << "Failed to parse configuration\n" << reader.getFormattedErrorMessages();
+		return -1;
+	}
+
+	std::cout << root << std::endl;
+
+	/*
 	Game g;
 	return g.execute();
+	*/
 }
