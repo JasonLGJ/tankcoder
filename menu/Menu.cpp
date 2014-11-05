3b7cd9d537af340eb929bca5053e000fd6c2e677
luissifu@xubuntu <luis.e.sifuentes@gmail.com>
2014-11-05 11:48:18 -0600

    even more stuff for menus

---
+++ menu/Menu.cpp
@@ -0,0 +1,23 @@
+#include "Menu.h"
+
+Menu::Menu() {}
+
+void Menu::load(std::string filename) {
+
+}
+
+void Menu::moved(float x, float y) {
+
+}
+
+void Menu::pressed() {
+
+}
+
+void Menu::released() {
+
+}
+
+void Menu::update() {
+
+}
